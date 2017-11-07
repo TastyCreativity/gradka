3b67e8216ceb33136d47bf1abb3d801360c0be13
Alexey Kirpichenkau <alexwish@tut.by>
2017-11-07 20:05:28 +0300

    block 7 is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,82 @@
-$(document).ready(function(){$(window).scroll(function(){$(window).width()>=1024&&(0==$(window).scrollTop()?$(".navbar").css("background-color","rgba(128, 128, 128, 1)"):$(".navbar").css("background-color","rgba(128, 128, 128, 0.2)"))}),$("#links__dropdown-button").click(function(){$("#links__dropdown-content a").toggleClass("links__show")}),window.onclick=function(o){if(!o.target.matches(".links__dropbtn")){var n,s=document.getElementsByClassName("links__dropdown-content");for(n=0;n<s.length;n++)s[n].classList.remove("links__show")}};$("ol.carousel-indicators li")});
+$(document).ready(function () {
+        $(window).scroll(function () {
+          if ($(window).width() >= 1024) {
+            if ($(window).scrollTop() == 0) {
+              $('.navbar').css('background-color', 'rgba(128, 128, 128, 1)');
+            } else {
+              $('.navbar').css('background-color', 'rgba(128, 128, 128, 0.2)');
+            }
+          }
+        })
+
+
+      $('#links__dropdown-button').click(function() {
+        $('#links__dropdown-content a').toggleClass("links__show");
+      });
+
+
+     window.onclick = function(event) {
+      if (!event.target.matches('.links__dropbtn')) {
+        var dropdowns = document.getElementsByClassName("links__dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          dropdowns[i].classList.remove('links__show');
+        }
+      }
+    }
+
+    var $carouselItems = $('ol.carousel-indicators li');
+
+    $('.history__navigation_prev').click(function(e) {
+      e.stopPropagation();
+      if($carouselItems.eq(1).hasClass('active')) {
+        $(this).addClass('disabled');
+        return false;
+      }
+      else {
+      $('.history__navigation_next').removeClass('disabled');
+      }
+
+      $carouselItems.each(function(index, el) {
+        var pos = $(this).offset().left;
+        var width = $(this).outerWidth(true);
+        $( this ).offset({ left : pos + width});
+      });
+
+      $('#history__carousel').carousel('prev');
+      if($carouselItems.eq(1).hasClass('active')) {
+        $(this).addClass('disabled');
+        $('.history__navigation_next').removeClass('disabled');
+      }
+
+      return false;
+    });
+
+    $('.history__navigation_next').click(function(e) {
+      e.stopPropagation();
+
+      if($carouselItems.eq($carouselItems.length - 2).hasClass('active')) {
+        $(this).addClass('disabled');
+        return false;
+      }
+      else {
+      $('.history__navigation_prev').removeClass('disabled');
+      }
+
+      $carouselItems.each(function(index, el) {
+        var pos = $(this).offset().left;
+        var width = $(this).outerWidth(true);
+        $( this ).offset({ left : pos - width});
+      });
+
+      $('#history__carousel').carousel('next');
+      if($carouselItems.eq($carouselItems.length - 2).hasClass('active')) {
+        $(this).addClass('disabled');
+        $('.history__navigation_prev').removeClass('disabled');
+      }
+      return false;
+    });
+
+
+
+});
